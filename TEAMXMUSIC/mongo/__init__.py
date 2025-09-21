ed90cddff45857c6dfca5249d10f8d5c587385a0
strad-dev131 <stradiogamer@gmail.com>
2025-09-22 01:12:31 +0530

    ..

--- TEAMXMUSIC/mongo/__init__.py
+++
@@ -1,9 +0,0 @@
-# Mongo package initialization
-from .afkdb import *
-from .couples_db import *
-from .pretenderdb import *
-from .welcomedb import *
-
-__all__ = [
-    "add_afk", "is_afk", "remove_afk",  # from afkdb
-]
